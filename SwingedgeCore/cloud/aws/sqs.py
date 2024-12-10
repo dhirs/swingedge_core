69f7dc508c896457f2ae6607e021770446d3b579
Dheeraj Saxena <dhirsaxena@gmail.com>
2024-12-10 12:33:12 +0000

    local changes

---
+++ SwingedgeCore/cloud/aws/sqs.py
@@ -0,0 +1,35 @@
+# Default params
+default_region_name = 'ap-south-1'
+
+# def send_msg_to_aws(msg):
+#     pass
+
+# import boto3
+# ## Get the service resource
+# sqs = boto3.resource('sqs')
+# def send_message_sqs(q_url, msg_body, message_attributes=None):
+#     queue_url = 'https://sqs.ap-south-1.amazonaws.com/863372932275/CandleData'
+#     resp = sqs.send_message(
+#     QueueUrl=queue_url,
+#     MessageBody=(
+#     msg_body
+#     )
+#     )
+    
+import boto3
+import json
+
+def sendToSQS(msg_body,msg_attr, queue_url,region_name=default_region_name):
+    sqs_client = boto3.client('sqs',region_name)
+    data = json.dumps(msg_body)
+    
+    try:
+        response = sqs_client.send_message(
+                QueueUrl=queue_url,
+                MessageBody = data
+            )
+        
+    except Exception as e:
+            error = f"Error sending message: {e}"
+            return error
+            
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SwingedgeCore',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['boto3'],
+    description='A package for utility functions used in SwingEdge Project',
+    url='https://github.com/dhirs/swingedge_core',
+)
