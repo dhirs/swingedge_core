ef8a1162a8fbec46934130c55265b0604a59c795
Dheeraj Saxena <dhirsaxena@gmail.com>
2024-12-10 17:22:59 +0000

    local changes

---
+++ SwingedgeCore/strategies/DB.py
@@ -0,0 +1,4 @@
+# Class which contains database specific information
+# this class contains methods to actually process the data
+# the sql to be processed will be passed by the calling
+# class/function
