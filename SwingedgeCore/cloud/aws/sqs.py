e3109d5e92664b12f414ee8e450e2cac49112f55
Dheeraj Saxena <dhirsaxena@gmail.com>
2024-12-11 17:41:57 +0000

    local changes

--- SwingedgeCore/cloud/aws/sqs.py
+++ SwingedgeCore/cloud/aws/sqs.py
@@ -1,21 +1,3 @@
-# Default params
-# default_region_name = 'ap-south-1'
-
-# def send_msg_to_aws(msg):
-#     pass
-
-# import boto3
-# ## Get the service resource
-# sqs = boto3.resource('sqs')
-# def send_message_sqs(q_url, msg_body, message_attributes=None):
-#     queue_url = 'https://sqs.ap-south-1.amazonaws.com/863372932275/CandleData'
-#     resp = sqs.send_message(
-#     QueueUrl=queue_url,
-#     MessageBody=(
-#     msg_body
-#     )
-#     )
-    
 import boto3
 import json
 
