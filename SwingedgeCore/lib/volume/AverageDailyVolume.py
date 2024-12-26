f0f28bdb1d05ac3d9bfbeb6a78652a5a2f3b8ef6
Dheeraj Saxena <dhirsaxena@gmail.com>
2024-12-26 07:12:29 +0000

    local changes

--- SwingedgeCore/lib/volume/AverageDailyVolume.py
+++ SwingedgeCore/lib/volume/AverageDailyVolume.py
@@ -23,4 +23,45 @@ def AverageDailyVolume(n=5, symbol):
     return response
 
 
-
+# This will return the Average Daily Volume for a specific date
+def AverageDailyVolumeNew(n=5, symbol, date=null):
+    
+    if date:
+        
+        date_of_query = date(date)
+        
+        # input t query will be symbol, date of query, limit
+        query = f"""
+        WITH daily_volume AS (
+            SELECT 
+                time_bucket(interval '1 day', timestamp) AS bucket,
+                SUM(volume) AS daily_volume
+            FROM stock_data
+            WHERE symbol = %s
+            and date(timestamp) = %s 
+            GROUP BY bucket
+            ORDER BY bucket DESC
+            LIMIT %s
+        )
+        
+    else:
+    
+        query = f"""
+        WITH daily_volume AS (
+            SELECT 
+                time_bucket(interval '1 day', timestamp) AS bucket,
+                SUM(volume) AS daily_volume
+            FROM stock_data
+            WHERE symbol = %s
+            GROUP BY bucket
+            ORDER BY bucket DESC
+            LIMIT %s
+        )
+        
+    SELECT 
+        ROUND(SUM(daily_volume) / %s, 3) AS average_volume
+    FROM daily_volume;
+    """
+    
+    response = DB.get_one(query,(symbol,n))
+    return response
