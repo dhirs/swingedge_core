17139a4874cb35f4469cb5ce02d34c0b31221f6f
AnurupaK <anurupakarmakar.dgp18@gmail.com>
2024-12-26 19:07:16 +0530

    volume files updated

--- SwingedgeCore/config/db.py
+++ SwingedgeCore/config/db.py
@@ -1,43 +1,60 @@
-import psycopg2
-import boto3
-from datetime import date
-import SwingedgeCore.cloud.aws.credentials as creds
+import boto3,json,psycopg2
 
-def get_creds()
+ssm = boto3.client('ssm')
+parameter = ssm.get_parameter(Name='timescaledb_credentials', WithDecryption=True)
+config = json.loads(parameter['Parameter']['Value'])
 
-    return creds.get_db_credentials():
+connection_established = False
 
-def get_connection():
-    config = get_creds()
-    
+def get_db_connection():
+    global connection_established
     try:
         conn = psycopg2.connect(
-            database=config['database'],
-            user=config['user'],
-            password=config['password'],
-            host=config['host'],
-            port=config['port'],
+            database = config['database'],
+            user =  config['user'],
+            password = config['password'],
+            host = config['host'],
+            port = config['port'],
         )
-        print("Connection to the Timescale PostgreSQL established successfully.")
+        if not connection_established:
+            print("Connection to the Timescale PostgreSQL established successfully.")
+            connection_established = True  
+        
         return conn
     except Exception as e:
-        print("Connection to the Timescale PostgreSQL encountered an error: ", e)
-        return None
-
-conn = get_connection()
-
-def get_one(query,params):
+        print("Connection to the Timescale PostgreSQL encountered an error: ",e)
+        return False
     
-    global conn
+def fetch_single_result(*,query, params):
+    conn = get_db_connection()
+    if not conn:
+        return None
     
     try:
         with conn.cursor() as cur:
-        cur.execute(query,params)
-        result = cur.fetchone()
-        return result[0] if result else None
-
+            cur.execute(query, params)
+            result = cur.fetchone()
+            return result[0] if result else None
     except Exception as e:
-        return e
+        print(f"Error executing query: {query}, Error: {e}")
+        return None
+    finally:
+        conn.close()
+        
+        
+def fetch_all_rows(*,query, params):
+    global conn  
+    try:
+        with conn.cursor() as cur:
+            cur.execute(query, params)
+            result = cur.fetchall()  
+            return result if result else []  
+    except Exception as e:
+        print("Error executing query in fetch_all_rows: ", e)
+        return None
+    finally:
+        conn.close()
+    
     
     
 def get_interval_days(start_date, end_date)
--- SwingedgeCore/lib/volume/TotalCurrentVolume.py
+++ SwingedgeCore/lib/volume/TotalCurrentVolume.py
@@ -1,32 +1,38 @@
-# import psycopg2
-# import pandas as pd
-# import boto3
-# import json
-# from datetime import datetime, timedelta
-import SwingEdgeCore.config.db as DB
+import sys
+from pathlib import Path
 
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+from config.db import fetch_single_result
 
-def TotalCurrentVolume(symbol="AAPL"):
-
-    # global conn
-
+def get_total_current_volume(symbol):
     try:
-              
-        query = """
-            SELECT SUM(volume) AS total_volume
-            FROM stock_data
-            WHERE symbol = %s
-            AND date(timestamp) = current_date;
+        # Query to fetch the latest timestamp for the given symbol in db
+        latest_timestamp_query = """
+        SELECT MAX(timestamp) 
+        FROM stock_data 
+        WHERE symbol = %s;
         """
+        latest_timestamp = fetch_single_result(query=latest_timestamp_query, params=(symbol,))
+        
+        if latest_timestamp is None:
+            return 0  
 
-        return DB.get_one(query,params)
-       
+        latest_date = latest_timestamp.date()
 
+        # Query to calculate the total volume for the latest date in db
+        total_volume_query = """
+        SELECT SUM(volume) 
+        FROM stock_data 
+        WHERE symbol = %s AND DATE(timestamp) = %s;
+        """
+        total_volume = fetch_single_result(query=total_volume_query, params=(symbol, latest_date))
+
+        return total_volume if total_volume is not None else 0
     except Exception as e:
-        print(f"An error occurred while calculating total current volume: {e}")
+        print("Error calculating total current volume: ", e)
         return None
-    
 
+##To do
 def TotalVolumeBetween(symbol, start_time, end_time)
 
     try:
