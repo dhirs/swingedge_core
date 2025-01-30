7d6bdaef748a91223ba4d0aa509a26dd2af862eb
Dheeraj Saxena <dhirsaxena@gmail.com>
2025-01-30 17:04:52 +0000

    local changes

--- SwingedgeCore/lib/utils/filters.py
+++ SwingedgeCore/lib/utils/filters.py
@@ -1,20 +1,13 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
 class Filters:
      def __init__(self,df):
          self.df = df
          
          
-     def close_price_filter(self):
+     def close_price_filter(self,min_price=None,max_price=None):
         data = self.df
         unique_symbols = data['symbol'].unique()
         
-        min_price = os.getenv("min_price")
-        max_price = os.genenv("max_price")
-        
+                
         for symbol in unique_symbols:
             symbol_data = data[data['symbol'] == symbol]
             
@@ -24,7 +17,7 @@ class Filters:
             # Minimum and maximum both specified
             if min_price is not None and max_price is not None:
                 if not (min_price <= min_timestamp_row['close'] <= max_price):
-                data = data[data['symbol'] != symbol]
+                    data = data[data['symbol'] != symbol]
                 
             # Minimum but no maximum
             if min_price is not None and max_price is None:
